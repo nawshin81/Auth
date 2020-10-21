134cfb55fd25afc82ef65be94ee13a6bc06a7635
nawshin81 <nawshinulfat@iut-dhaka.edu>
2020-10-22 03:20:03 +0600

    Authentication done

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,19 +1,19 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Input, Button} from "react-native-elements";
-import { AuthContext } from "./src/providers/AuthProvider";
-
+import { AuthContext } from "../providers/AuthProvider";
 const HomeScreen = (props) => {
   return (
     <AuthContext.Consumer>
       {(auth) => (
         <View>
-          <Text style={styles.textStyle}>Home Screen</Text>
+          <Text style={styles.textStyle}>Welcome {auth.CurrentUser.name} !</Text>
           <Button
             title="Log Out"
             type="outline"
             onPress={function () {
               auth.setIsLoggedIn(false);
+              auth.setCurrentUser({})
             }}
           />
         </View>
